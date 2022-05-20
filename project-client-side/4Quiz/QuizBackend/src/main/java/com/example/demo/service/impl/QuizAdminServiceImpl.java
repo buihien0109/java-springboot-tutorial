a64ee1b6e2db106980be37d3e0e40dec75bee0d4
buihien0109 <buihien01091997@gmail.com>
2022-05-20 23:46:04 +0700

    update

--- project-client-side/4Quiz/QuizBackend/src/main/java/com/example/demo/service/impl/QuizAdminServiceImpl.java
+++ project-client-side/4Quiz/QuizBackend/src/main/java/com/example/demo/service/impl/QuizAdminServiceImpl.java
@@ -12,12 +12,14 @@ import java.util.*;
 
 @Service
 public class QuizAdminServiceImpl implements QuizAdminService {
+    // Tạo danh sách quiz để quản lý
     private List<Quiz> quizzes = new ArrayList<>();
 
     public QuizAdminServiceImpl() {
         init();
     }
 
+    // Thêm 1 số câu hỏi và câu trả lời ban đầu cho danh sách
     @Override
     public void init() {
         Random rd = new Random();
@@ -71,13 +73,16 @@ public class QuizAdminServiceImpl implements QuizAdminService {
         quizzes.addAll(Arrays.asList(quiz1, quiz2, quiz3, quiz4, quiz5));
     }
 
+    // Lấy danh sách quiz
     @Override
     public List<Quiz> getAllQuiz() {
         return quizzes;
     }
 
+    // Lấy chi tiết câu hỏi theo id
     @Override
     public Quiz getQuizById(int id) {
+        // Kiểm tra quiz có tồn tại hay không
         Optional<Quiz> quizOptional = findQuiz(id);
         if (quizOptional.isEmpty()) {
             throw new NotFoundException("quiz with id = " + id + " is not exist!");
@@ -86,15 +91,18 @@ public class QuizAdminServiceImpl implements QuizAdminService {
         return quizOptional.get();
     }
 
+    // Tạo câu hỏi mới
     @Override
     public Quiz createQuiz(CreateQuizRequest request) {
         Random rd = new Random();
 
+        // Tạo câu hỏi
         Quiz quiz = new Quiz();
         quiz.setId(rd.nextInt(1000));
         quiz.setTitle(request.getTitle());
         List<QuizAnswer> quizAnswers = new ArrayList<>();
 
+        // Tạo câu trả lời
         for (QuizAnswer quizAnswerRequest : request.getQuizAnswers()) {
             QuizAnswer quizAnswer = new QuizAnswer();
             quizAnswer.setId(rd.nextInt(1000));
@@ -102,12 +110,15 @@ public class QuizAdminServiceImpl implements QuizAdminService {
             quizAnswer.setCorrect(quizAnswerRequest.isCorrect());
             quizAnswers.add(quizAnswer);
         }
-
         quiz.setQuizAnswers(quizAnswers);
+
+        // Thêm câu hỏi vào list để quản lý
         quizzes.add(quiz);
+
         return quiz;
     }
 
+    // Cập nhật câu hỏi
     @Override
     public Quiz updateQuiz(int id, UpdateQuizRequest request) {
         // Kiểm tra quiz có tồn tại hay không
@@ -118,71 +129,89 @@ public class QuizAdminServiceImpl implements QuizAdminService {
 
         // Lấy ra nội dung quiz
         Quiz quiz = quizOptional.get();
+
         // Đặt lại title cho quiz
         quiz.setTitle(request.getTitle());
+
+        // Xóa hết các phần tử trong danh sách đáp án -> sau đó set lại
         quiz.getQuizAnswers().clear();
         quiz.setQuizAnswers(request.getQuizAnswers());
 
         return quiz;
     }
 
+    // Xóa câu hỏi theo id
     @Override
     public void deleteQuiz(int id) {
+        // Kiểm tra quiz có tồn tại hay không
         if (findQuiz(id).isEmpty()) {
             throw new NotFoundException("quiz with id = " + id + " is not exist!");
         }
+
+        // Nếu câu hỏi tồn tại thì tiến hành xóa
         quizzes.removeIf(quiz -> quiz.getId() == id);
     }
 
+    // Tạo câu trả lời mới
     @Override
     public QuizAnswer createQuizAnswer(int quizId) {
+        // Kiểm tra quiz có tồn tại hay không
         Optional<Quiz> quizOptional = findQuiz(quizId);
         if (quizOptional.isEmpty()) {
             throw new NotFoundException("quiz with id = " + quizId + " is not exist!");
         }
 
+        // Lấy ra thông tin của câu hỏi
         Quiz quiz = quizOptional.get();
 
+        // Tạo câu trả lời
         Random rd = new Random();
         QuizAnswer quizAnswer = new QuizAnswer();
         quizAnswer.setId(rd.nextInt(1000));
         quizAnswer.setTitle("");
         quizAnswer.setCorrect(false);
 
+        // Thêm câu trả lời mới vào danh sách câu trả lời
         quiz.getQuizAnswers().add(quizAnswer);
 
+        // Trả về câu trả lời mới sau khi tạo thành công
         return quizAnswer;
     }
 
+    // Xóa câu trả lời
     @Override
     public void deleteQuizAnswer(int quizId, int quizAnswerId) {
+        // Kiểm tra quiz có tồn tại hay không
         if (findQuiz(quizId).isEmpty()) {
             throw new NotFoundException("quiz with id = " + quizId + " is not exist!");
         }
 
+        // Kiểm tra câu trả lời có tồn tại hay không
         if (findQuizAnswerById(quizId, quizAnswerId).isEmpty()) {
             throw new NotFoundException("quiz answers with id = " + quizAnswerId + " is not exist!");
         }
 
+        // Xóa câu trả lời
         findQuiz(quizId)
                 .get()
                 .getQuizAnswers()
                 .removeIf(quizAnswer -> quizAnswer.getId() == quizAnswerId);
     }
-    
+
+    // HELPER METHOD : Tìm kiếm câu hỏi theo id
     public Optional<Quiz> findQuiz(int id) {
         return quizzes
                 .stream()
                 .filter(quiz -> quiz.getId() == id)
-                .findAny();
+                .findFirst();
     }
 
-
+    // HELPER METHOD : Tìm kiếm câu hỏi theo id của câu hỏi, và id của câu trả lời
     public Optional<QuizAnswer> findQuizAnswerById(int quizId, int quizAnswerId) {
         Quiz quiz = findQuiz(quizId).get();
         return quiz.getQuizAnswers()
                 .stream()
                 .filter(quizAnswer -> quizAnswer.getId() == quizAnswerId)
-                .findAny();
+                .findFirst();
     }
 }
--- project-client-side/4Quiz/QuizBackend/src/main/java/com/example/demo/service/impl/QuizUserServiceImpl.java
+++ project-client-side/4Quiz/QuizBackend/src/main/java/com/example/demo/service/impl/QuizUserServiceImpl.java
@@ -15,21 +15,31 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class QuizUserServiceImpl implements QuizUserService {
-    private QuizAdminService quizAdminService;
+    private final QuizAdminService quizAdminService;
 
+    // Lấy danh sách quiz, đảo vị trí các câu hỏi và câu trả lời
     @Override
     public List<Quiz> getAllQuiz() {
+        // Lấy danh sách quiz
         List<Quiz> quizzes = quizAdminService.getAllQuiz();
+
+        // Đảo vị trí các câu hỏi
         Collections.shuffle(quizzes);
+
+        // Đảo vị trí các câu trả lời
         for (Quiz quiz: quizzes) {
             Collections.shuffle(quiz.getQuizAnswers());
         }
         return quizzes;
     }
 
+    // Kiểm tra đáp án của người chơi có chính xác hay không
     @Override
     public boolean checkQuiz(int id, CheckQuizRequest request) {
+        // Lấy ra câu hỏi theo id
         Quiz quiz = quizAdminService.getQuizById(id);
+
+        // Kiểm tra câu trả lời của người chơi với câu trả lời của câu hỏi
         for (QuizAnswer qa: quiz.getQuizAnswers()) {
             if(qa.getId() == request.getId()) {
                 return qa.isCorrect();
